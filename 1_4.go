3a6652a34c9cd4a266b530952b4c3fbb0e905981
Yusuke Sangenya <sangenya@sancha-mac.local>
2016-11-06 22:17:35 +0900

    1_4 second

--- 1_4.go
+++ 1_4.go
@@ -6,23 +6,34 @@ import (
 )
 
 func main() {
-	newton_f1()
-	newton_f2()
+	fmt.Print("f1\t")
+	newton(f1, f1_prime, 0.0001, 0.1)
+	fmt.Print("f2\t")
+	newton(f2, f2_prime, 0.0001, 0.1)
 }
 
-func newton_f1() {
-	var x_prev float64 = 0.1
+// newton(f, f_prime, e, x0)
+//   f       : 対象関数
+//   f_prim  : fの一次導関数
+//   e       : "|x_(k+1) - x_k| < e" が満たされたときに終了する
+//   x0      : x0
+// 表示される値
+//   k    : "|x_k - x_(k-1)| < e"が満たされた、最初のkの値
+//   x    : x_kの値
+//   f(x) : f(x_k)の値
+func newton(f func(float64) float64, f_prime func(float64) float64, e float64, x0 float64) {
+	var x_prev float64 = x0
 	var x_next float64
 
-	i := 0
+	k := 0
 	for {
-		x_next = x_prev - f1(x_prev)/f1_prime(x_prev)
-		if math.Abs(x_next-x_prev) < 0.0001 {
-			fmt.Printf("i=%v, x=%v, f1(x)=%v\n", i, x_next, f1(x_next))
+		x_next = x_prev - f(x_prev)/f_prime(x_prev)
+		if math.Abs(x_next-x_prev) < e {
+			fmt.Printf("k=%v, x=%v, f(x)=%v\n", k, x_next, f(x_next))
 			break
 		}
 		x_prev = x_next
-		i++
+		k++
 	}
 }
 
@@ -32,22 +43,6 @@ func f1(x float64) float64 {
 
 func f1_prime(x float64) float64 {
 	return -8.8*math.Pow(x, 3) + 10.5*math.Pow(x, 2) + 8.2*x + 3.3
-}
-
-func newton_f2() {
-	var x_prev float64 = 0.1
-	var x_next float64
-
-	i := 0
-	for {
-		x_next = x_prev - f2(x_prev)/f2_prime(x_prev)
-		if math.Abs(x_next-x_prev) < 0.0001 {
-			fmt.Printf("i=%v, x=%v, f2(x)=%v\n", i, x_next, f2(x_next))
-			break
-		}
-		x_prev = x_next
-		i++
-	}
 }
 
 func f2(x float64) float64 {
