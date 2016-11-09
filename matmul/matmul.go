58f7a7df58213bbee87b0273f8b0471c80ed7f33
Yusuke Sangenya <sangenya@h1.251.225.10.1017429.vlan.kuins.net>
2016-11-10 01:32:37 +0900

    add comment

--- matmul/matmul.go
+++ matmul/matmul.go
@@ -1,50 +1,83 @@
+// 後で他のソースコードから関数をモジュールとして参照するので、パッケージをexportしている。
+// Go言語では、初めの文字が大文字の関数が公開され、小文字の関数は公開されない。
 package matmul
 
-import "github.com/k0kubun/pp"
+// Println関数を使うためにパッケージをimportしている。
+import "fmt"
 
-// i => 行, j => 列とする
-/* ex:)
+// #define N 3 の代わり
+const N = 3
 
-[[1,2,3]
-,[4,5,6]
-,[7,8,9]]
+/* これ以降、行列の添字としては、行 => i, 列 => jを用いる。
+	ex:)
 
-i = 1
-j = 2
- => 6
+	行列を、
+		[[1,2,3]
+		,[4,5,6]
+		,[7,8,9]]
+	として、6を参照するには
+		i = 1
+		j = 2
+	とする。
 */
 
-const N = 6
-
+// この関数の中で動作確認をしている
 func main() {
+	// 行列を定義
+	/*
+		|1 4 7|
+		|2 5 8|
+		|3 6 9|
+	*/
 	A := [N][N]float64{
 		{1, 4, 7},
 		{2, 5, 8},
 		{3, 6, 9},
 	}
+	// ベクトルを定義
 	b1 := [N]float64{1, 2, 3}
 	b2 := [N]float64{4, 5, 6}
 	b3 := [N]float64{7, 8, 9}
 
+	// 行列ベクトル積を計算し、
+	// 結果のベクトルから行列を作成している
+	/* 転置を取っていることについて。	
+		転置を取らずに行列を作成すると、行列ベクトル積の結果をv1, v2, v3としたとき、
+			(v1 v2 v3)
+		のような行列が作成されてしまう。得たい行列は
+			|v1|
+			|v2|
+			|v3|
+		のような行列である。この２つの行列は、転置の関係にあるので、転置を取ってA(b1 b2 b3)の計算結果としている。
+	*/
 	Abs := Transpose([N][N]float64{
 		MatVec(A, b1),
 		MatVec(A, b2),
 		MatVec(A, b3),
 	})
 
+	// 行列積を計算
 	AA := MatMlt(A, A)
 
-	pp.Println(Abs)
-	pp.Println(AA)
+	// Goの==は、Arrayに適用された場合は、
+	// ２つのArrayの対応する要素の値がすべて等しいときにtrueを返す。
+	if (AA == Abs) {
+		fmt.Println("AA == Abs")
+	}
 
+	// 行列積を取ってからベクトル積を取った場合
 	AA_b1 := MatVec(MatMlt(A, A), b1)
+	// ベクトル積を取ってから、更にもう一度ベクトル積を取った場合
 	A_Ab1 := MatVec(A, MatVec(A, b1))
 
-	pp.Println(AA_b1)
-	pp.Println(A_Ab1)
+	// Goの==は、Arrayに適用された場合は、
+	// ２つのArrayの対応する要素の値がすべて等しいときにtrueを返す。
+	if (AA_b1 == A_Ab1) {
+		fmt.Println("AA_b1 == A_Ab1")
+	}
 }
 
-// 行列とベクトルを受け取り、ベクトルを返す
+// 行列ベクトル積を計算する関数
 func MatVec(mat [N][N]float64, vec [N]float64) [N]float64 {
 	var ret_vec [N]float64
 
@@ -56,14 +89,14 @@ func MatVec(mat [N][N]float64, vec [N]float64) [N]float64 {
 	return ret_vec
 }
 
-// 行列と行列を受け取り、行列を返す
+// 行列積を計算する関数
 func MatMlt(mat1 [N][N]float64, mat2 [N][N]float64) [N][N]float64 {
 	var ret_mat [N][N]float64
 
 	for i := 0; i < N; i++ {
 		for j := 0; j < N; j++ {
 			// 行列1のi行ベクトルと、行列2のj列ベクトルのベクトル積が、
-			// 返す行列のi行j列成分
+			// 返す行列のi行j列成分であることを利用して計算している
 			ret_mat[i][j] = dot(mat1[i], fetch_col(mat2, j))
 		}
 	}
@@ -71,7 +104,7 @@ func MatMlt(mat1 [N][N]float64, mat2 [N][N]float64) [N][N]float64 {
 	return ret_mat
 }
 
-// ベクトルとベクトルを受け取り、スカラーを返す
+// ベクトルの内積を取る関数
 func dot(vec1 [N]float64, vec2 [N]float64) float64 {
 	var sum float64 = 0
 	for n := 0; n < N; n++ {
@@ -80,8 +113,7 @@ func dot(vec1 [N]float64, vec2 [N]float64) float64 {
 	return sum
 }
 
-// 行列とスカラーを受け取り、ベクトルを返す
-// 行列から列ベクトルを取り出す
+// 行列から列ベクトルを取り出す関数
 func fetch_col(mat [N][N]float64, col_ind int) [N]float64 {
 	var col [N]float64
 
@@ -92,6 +124,7 @@ func fetch_col(mat [N][N]float64, col_ind int) [N]float64 {
 	return col
 }
 
+// 行列の転置を取る関数
 func Transpose(mat [N][N]float64) [N][N]float64 {
 	var ret_mat [N][N]float64
 
