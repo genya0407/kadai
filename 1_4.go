7859c2aade2e4ed93b2e8d7be71042e6f8e5d3db
Yusuke Sangenya <sangenya@h1.251.225.10.1017429.vlan.kuins.net>
2016-11-10 03:08:10 +0900

    add comment again

--- 1_4.go
+++ 1_4.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"math"
+	"fmt" // Println関数
+	"math" // SmallestNonzeroFloat64定数, 
 )
 
 func main() {
@@ -12,7 +12,7 @@ func main() {
 	newton(f2, f2_prime, math.SmallestNonzeroFloat64, 0.1)
 }
 
-// newton(f, f_prime, e, x0)
+// newton(f, f_prime, e, x0)の引数
 //   f       : 対象関数
 //   f_prim  : fの一次導関数
 //   e       : "|x_(k+1) - x_k| < e && f(x_(k+1)) < e" が満たされたときに終了する
@@ -22,27 +22,33 @@ func main() {
 //   x    : x_kの値
 //   f(x) : f(x_k)の値
 // 挙動の解説
-//   "|x_(k+1) - x_k| < e && f(x_(k+1)) < e" が初めて満たされたときに、その時点でのk, x, f(x)を終了する。
-//   "|x_(k+1) - x_k| < e"か"|f(x_(k+1))| < e"が初めて満たされたとき、その時点でのk, x, f(x)を表示する。
-// 　"|x_(k+1) - x_k| < e"が初めて満たされたときは、表示される行の末尾に":: delta x < e satisfied"が追加される。
-//   "|f(x_(k+1))| < e"が初めて満たされたときは、表示される行の末尾に":: f(x) < e satisfied"が追加される。
+//   1. "|x_(k+1) - x_k| < e && f(x_(k+1)) < e" が初めて満たされたときに、その時点でのk, x, f(x)を終了する。
+//   2. "|x_(k+1) - x_k| < e"が満たされたとき、その時点でのk, x, f(x)を表示する。また、表示される行の末尾に":: delta x < e satisfied"が追加される。
+//   3. "|f(x_(k+1))| < e"が満たされたとき、その時点でのk, x, f(x)を表示する。また、表示される行の末尾に":: f(x) < e satisfied"が追加される。
+// なお、ステップの実行回数は100回までとした。
 func newton(f func(float64) float64, f_prime func(float64) float64, e float64, x0 float64) {
-	var x_prev float64 = x0
+	/*
+		各ステップでのx_kを表しているのがx_prev変数
+		各ステップでのx_k+1を表しているのがx_next変数
+	*/
+	var x_prev float64
 	var x_next float64
 
 	k := 0
-	fx := true
-	diff_x := true
-	for {
+	x_prev = x0
+	for k < 100 {
+		// 修正量を計算し、それから反復列の次の近似解を求める
 		x_next = x_prev - f(x_prev)/f_prime(x_prev)
-		if math.Abs(f(x_next)) < e && fx {
+
+		// 途中経過を表示する
+		if math.Abs(f(x_next)) < e {
 			fmt.Printf("k=%v, x=%v, f(x)=%v :: f(x) < e satisfied\n", k, x_next, f(x_next))
-			fx = false
 		}
-		if math.Abs(x_next-x_prev) < e && diff_x {
+		if math.Abs(x_next-x_prev) < e {
 			fmt.Printf("k=%v, x=%v, f(x)=%v :: delta x < e satisfied\n", k, x_next, f(x_next))
-			diff_x = false
 		}
+
+		// 終了条件を両方共満たしたとき終了する
 		if math.Abs(x_next-x_prev) < e && math.Abs(f(x_next)) < e {
 			fmt.Printf("k=%v, x=%v, f(x)=%v\n", k, x_next, f(x_next))
 			break
@@ -52,18 +58,24 @@ func newton(f func(float64) float64, f_prime func(float64) float64, e float64, x
 	}
 }
 
+// 問に挙げられた方程式のうち、4次方程式の方を表す関数
+// xを受け取り、関数を評価した値を返す
 func f1(x float64) float64 {
 	return -2.2*math.Pow(x, 4) + 3.5*math.Pow(x, 3) + 4.1*math.Pow(x, 2) + 3.3*x - 2.7
 }
 
+// f1の一次導関数を表す
 func f1_prime(x float64) float64 {
 	return -8.8*math.Pow(x, 3) + 10.5*math.Pow(x, 2) + 8.2*x + 3.3
 }
 
+// 問に挙げられた方程式のうち、余弦関数を含む方を表す関数
+// xを受け取り、関数を評価した値を返す
 func f2(x float64) float64 {
 	return -3*math.Cos(2*x+2) + math.Exp(x+1) - 2*x - 30
 }
 
+// f2の一次導関数を表す
 func f2_prime(x float64) float64 {
 	return 6*math.Sin(2*x+2) + math.Exp(x+1) - 2
 }
