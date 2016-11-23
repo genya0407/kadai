7e14fdc78ba7c2f8f619bbb2a681f54f9693d295
Yusuke Sangenya <sangenya@sancha-mac.local>
2016-11-24 07:07:34 +0900

    6

---
+++ second/6.go
@@ -0,0 +1,27 @@
+package main
+
+import "math"
+import "fmt"
+
+func main() {
+	var H [N][N]float64
+	var b [N]float64
+
+	for i := 0; i <= N-1; i++ {
+		for j := 0; j <= N-1; j++ {
+			H[i][j] = math.Pow(0.25, math.Abs(float64(i-j)))
+		}
+	}
+	for i := 0; i <= N-1; i++ {
+		b[i] = 5.0 - math.Pow(4.0, float64(i-N+1)) - math.Pow(4.0, float64(-i))
+	}
+
+	x := Solve(H, b)
+	PrintVector(x)
+}
+
+func PrintVector(vector [N]float64) {
+	for i := 0; i <= N-1; i++ {
+		fmt.Printf("%3.3f\n", vector[i])
+	}
+}
---
+++ second/6_1.go
@@ -0,0 +1,3 @@
+package main
+
+const N = 3
