aa47c106397e790eb14e8b6655281d4b011ce3eb
Yusuke Sangenya <sangenya@sancha-mac.local>
2016-11-24 07:59:55 +0900

    add command list

---
+++ second/commands.sh
@@ -0,0 +1,8 @@
+go run 1.go forward.go
+go run 2.go backward.go
+go run 4.go lu.go
+go run 5.go solve.go lu.go forward.go backward.go
+go run 6_1.go 6.go solve.go lu.go forward.go backward.go
+go run 6_2.go 6.go solve.go lu.go forward.go backward.go
+go run 8_1.go 8.go inverse.go solve.go lu.go forward.go backward.go
+go run 8_2.go 8.go inverse.go solve.go lu.go forward.go backward.go
