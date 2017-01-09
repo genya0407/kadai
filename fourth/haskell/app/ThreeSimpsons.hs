a5fd203c8b5540335bbcab6d82c14feaba19253f
Yusuke Sangenya <longinus.eva@gmail.com>
2017-01-10 01:02:50 +0900

    finish refactoring

--- fourth/haskell/app/ThreeSimpsons.hs
+++ fourth/haskell/app/ThreeSimpsons.hs
@@ -1,6 +1,7 @@
 module Main where
 
-import           Three
+import           Three (calc)
 import qualified Seq as S
 
+-- 複合シンプソン公式によってπを求める
 main = calc S.compositSimpsonsRule
--- fourth/haskell/app/ThreeTrapezoidal.hs
+++ fourth/haskell/app/ThreeTrapezoidal.hs
@@ -1,6 +1,7 @@
 module Main where
 
-import           Three
+import           Three (calc)
 import qualified Seq as S
 
+-- 複合台形公式によってπを求める
 main = calc S.compositTrapezoidalRule
